f620da129ccd16be50bc60141132e3532bf108f7
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-06-14 22:38:02 +0200

    initialize project

---
+++ review_dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+
+default_arguments = {
+    'owner': "frils",
+    'start_date': datetime(2021, 5, 21, 0, 0, 0),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=10)
+}
+
+dag = DAG(
+    dag_id="review_dag",
+    max_active_runs=5,
+    schedule_interval='0 * * * *',
+    default_args=default_arguments,
+    catchup=False
+)
