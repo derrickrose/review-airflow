e09d3f878f662f05792af46d72e03812732eac02
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-06-17 21:52:48 +0200

    update code

--- dev/airflow_plugins/operators/review_operator.py
+++ dev/airflow_plugins/operators/review_operator.py
@@ -1,14 +1,14 @@
-from airflow.models import BaseOperator
-from airflow.utils import apply_defaults
-import logging as log
-
-
-class ReviewOperator(BaseOperator):
-
-    @apply_defaults
-    def __init__(self, param, *args, **kwargs):
-        super.__init__(args, kwargs)
-        self.param = param
-
-    def execute(self, context):
-        log.info("{} execute with param {}".format(self.__class__.__name__, self.param))
+# from airflow.models import BaseOperator
+# from airflow.utils import apply_defaults
+# import logging as log
+#
+#
+# class ReviewOperator(BaseOperator):
+#
+#     @apply_defaults
+#     def __init__(self, param, *args, **kwargs):
+#         super.__init__(args, kwargs)
+#         self.param = param
+#
+#     def execute(self, context):
+#         log.info("{} execute with param {}".format(self.__class__.__name__, self.param))
