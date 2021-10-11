e110b708ad5ca6cc34d70741c7c78c0152aeb76c
derrickrose <randofrils@gmail.com>
2021-10-11 21:39:54 +0200

    update installation of airflow in wsl ubuntu 20

--- wsl/ubuntu-20/airflow_wsl_install.sh
+++ wsl/ubuntu-20/airflow_wsl_install.sh
@@ -8,25 +8,45 @@ sudo apt-get update -y
 sudo apt-get install python3-pip -y
 
 ## install airflow
-pip3 install apache-airflow
+#pip3 install apache-airflow
+AIRFLOW_VERSION=2.1.2
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+export PATH=$PATH:/home/frils/.local/bin/
+pip3 install testresources
+pip3 install "apache-airflow[async,postgres,google]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
 ## create airflow home directory
-mkdir -p /home/frils/airflow_home
+mkdir -p /home/frils/airflow
 
 ## make environment variable airflow_home
-export AIRFLOW_HOME=/home/frils/airflow_home
+export AIRFLOW_HOME=/home/frils/airflow
 
-## test airflow installation
-airflow info
+#install airflow dependencies for amazon
+pip install apache-airflow-providers-amazon
 
-#!/bin/bash
-
-
-## automount and link drives to / 
+## automount and link drives to /
 WSL_CONF_FILE_NAME=wsl.conf
 WSL_CONF_PATH="/etc/$WSL_CONF_FILE_NAME"
-echo "[automount]" > $WSL_CONF_FILE_NAME
-echo "root = /" >> $WSL_CONF_FILE_NAME
-echo "options = \"metadata\"" >> $WSL_CONF_FILE_NAME
-
+echo "[automount]" >$WSL_CONF_FILE_NAME
+echo "root = /" >>$WSL_CONF_FILE_NAME
+echo "options = \"metadata\"" >>$WSL_CONF_FILE_NAME
 sudo mv $WSL_CONF_FILE_NAME $WSL_CONF_PATH
+
+# show airflow conf
+airflow info
+
+# start airflow db
+airflow db init
+
+# create an airflow user
+airflow users create \
+  --username admin \
+  --firstname Peter \
+  --lastname Parker \
+  --role Admin \
+  --password admin \
+  --email spiderman@superhero.org
+
+# show airflow conf
+airflow info
