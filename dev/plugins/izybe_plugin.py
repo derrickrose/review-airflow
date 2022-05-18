f1c63227a44158cd111c96379f980a8ff070cfff
Frelin AMPILAHY <randofrils@gmail.com>
2022-05-18 23:55:42 +0200

    test

--- dev/plugins/izybe_plugin.py
+++ dev/plugins/izybe_plugin.py
@@ -1,8 +1,9 @@
 from airflow.plugins_manager import AirflowPlugin
 
 
-class ReviewPlugin(AirflowPlugin):
-    name = "ReviewPlugin"
+class IzybePlugin(AirflowPlugin):
+    name = "IzybePlugin"
+    version = "0.0.1"
     operators = []
     hooks = []
     sensors = []
