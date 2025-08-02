cc308b6fbffadd4ffbe167dc0b4602cce118013f
frils <randofrils@gmail.com>
2025-08-02 17:44:29 +0200

    update

--- enough_python/dsa_group/03_binary_trees_and_bsts/warmup_binary_tree.py
+++ enough_python/dsa_group/03_binary_trees_and_bsts/warmup_binary_tree.py
@@ -1,80 +1,68 @@
 class Node:
-    def __init__(self, item, left=None, right=None):
+    def __init__(self, item):
         self.item = item
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
     def __repr__(self):
-        return f"{self.item}"
+        return self.item
 
     def __str__(self):
-        return f"{self.item} -> {self.left} , {self.right}"
+        return self.item
 
     def has_children(self):
-        return self.left or self.right
-
-    def has_left_child(self):
-        return self.left
-
-    def has_right_child(self):
-        return self.right
-
-    def is_leaf(self):
-        return not self.left and not self.right
+        return self.left is not None or self.right is not None
 
     def get_children(self):
         return [self.left, self.right]
 
-    #
-    # def get_grand_children_flat(self):
-    #     return [gc for child in self.get_children() if child for gc in child.get_children()]
-    # def get_grand_children(self):
-    #     grand_children = []
-    #     for child in self.get_children():
-    #         if child:
-    #             grand_children.extend(child.get_children())
-    #     return grand_children
+    def has_grand_children(self):
+        return self.left and self.left.has_children() or self.right and self.right.has_children()
 
-    def get_grand_children(self):
-        return [gc if child else None for child in self.get_children() for gc in child.get_children()]
-
-    def get_level(self):
-        size = 1
+    def get_size(self):
         left_size = 0
         right_size = 0
         if self.left:
             left_size = self.left.get_size()
         if self.right:
             right_size = self.right.get_size()
-        return size + max(left_size, right_size)
+        return left_size + right_size + 1
 
-    def get_size(self):
-        size = 1
+    def get_depth(self):
+        left_depth = 0
+        right_depth = 0
         if self.left:
-            size += self.left.get_size()
+            left_depth = self.left.get_depth()
         if self.right:
-            size += self.right.get_size()
-        return size
+            right_depth = self.right.get_depth()
+        return max(left_depth, right_depth) + 1
+
+    def get_grand_children(self):
+        if self.has_grand_children():
+            return [gc for child in self.get_children() if child for gc in child.get_children() if gc]
+        return []
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
 
 
 a = Node("A")
 b = Node("B")
-c = Node("C")
-u = Node("U")
-v = Node("V")
-w = Node("W")
-x = Node("X")
-
 a.left = b
+c = Node("C")
 a.right = c
-b.left = v
+u = Node("U")
 b.right = u
+v = Node("V")
+b.left = v
+w = Node("W")
 v.left = w
+x = Node("X")
 v.right = x
 
-print(a.get_level())
 print(a.get_size())
+print(a.get_depth())
+print(a.get_grand_children())
+print(v.has_grand_children())
 print(a.is_leaf())
-print(b.get_children())
-print("   __ ")
-print(b.get_grand_children())
+print(v.is_leaf())
