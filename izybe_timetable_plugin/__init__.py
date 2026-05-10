667a33991968e6138171b03bc2f71f365b3a3d2f
Andrianarisoa Ampilahy <randofrils@gmail.com>
2026-05-11 00:06:50 +0200

    aaa

--- izybe_timetable_plugin/__init__.py
+++
@@ -1,9 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-
-from old_plugins.izybe_timetable_plugin.daily import DailyTimetable
-
-
-class IzybeTimetablePlugin(AirflowPlugin):
-    name = "izybe_timetable_plugin"
-    timetables = [DailyTimetable]
-
